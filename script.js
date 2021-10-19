8fcbd33683f4302f550d5a9a3bfbf718882fb565
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2021-10-19 05:21:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// enable strict mode
+"use strict";
+// write variables you will use throughout file
+const daily = document.querySelector(".section__body-containerdivp");
+const weekly = document.querySelector(".section__body-containerdivp2");
+const monthly = document.querySelector(".section__body-containerdivp3");
+// write event handler for click in daily
+daily.addEventListener("click", function () {
+  // write code for javascript reading from json file
+});
